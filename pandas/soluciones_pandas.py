386b8d5e5d8dcfdbb412445d507130e642946e1a
nick-290811 <nicolas.salgado@est.uexternado.edu.co>
2026-05-18 23:11:28 -0500

    agregado pandas decil final

--- pandas/soluciones_pandas.py
+++ pandas/soluciones_pandas.py
@@ -12,3 +12,17 @@ df = pd.read_csv(csv_path, index_col=0)
 # SOLUCIONES DE LOS EJERCICIOS DE PANDAS
 # ==========================================
 
+def DecilFinal(estudiante):
+    """
+    Recibe el nombre de un estudiante y retorna el decil de su nota final.
+    """
+    # Calcular la nota final como el promedio de los 4 parciales
+    notas_finales = df[['parcial1', 'parcial2', 'parcial3', 'parcial4']].mean(axis=1)
+    
+    # Calcular deciles del 1 al 10
+    deciles = pd.qcut(notas_finales, q=10, labels=False, duplicates='drop') + 1
+    
+    if estudiante in deciles.index:
+        return deciles[estudiante]
+    else:
+        return None
