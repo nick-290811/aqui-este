c4cb274a840ffa9fb596494f3ad6613be1234be8
Arthur88-05 <arthur.araujo@est.uexternado.edu.co>
2026-05-19 03:22:45 +0000

    Solución Ejercicios 3 y 4

--- parcial1/soluciones_p1.py
+++ parcial1/soluciones_p1.py
@@ -21,3 +21,48 @@ def f2(L):
 # Ejemplo de uso: si L = [1, 2, 3], entonces f(x) = 1 + 2*x + 3*x^2
 # polinomio = f2([1, 2, 3])
 # print(polinomio(2))  # Evaluar el polinomio en x=2, debería retornar 1 + 2*2 + 3*2^2 = 1 + 4 + 12 = 17
+
+
+
+#Ejercicio 3. Escribir una función f3(x0, y0) que recibe dos números y retorna dos funciones recta(x) y paralela(x).
+# La función recta(x) representa la recta que pasa por (x0, y0) y tiene pendiente 2.
+#  La función paralela(x) representala recta de pendiente 2 que pasa por (1, 1).
+
+
+def f3(x0, y0):
+
+    def recta(x):
+        return 2 * (x - x0) + y0
+
+    def paralela(x):
+        return 2 * (x - 1) + 1
+
+    return recta, paralela
+
+# Ejemplo de uso recta, paralela = f3(2, 2)
+# recta(3), paralela(3)
+# > (4, 5)
+
+#Ejercicio 4. Escribir una función f4(L) que retorna la suma de los elementos en L que son mayores que 10 y la 
+# suma de su primer y  ultimo dígito es par.          
+
+def f4(L):
+    suma = 0
+
+    for num in L:
+        if num > 10:
+
+            texto = str(num)
+
+            primero = int(texto[0])
+            ultimo = int(texto[-1])
+
+            if (primero + ultimo) % 2 == 0:
+                suma += num
+
+    return suma
+#Ejemplo de uso 
+# f4([12, 15, 23, 34, 45, 56, 11])
+# 15 (1+5=6) + 11 (1+1=2) = 26
+# > 26
+# 
