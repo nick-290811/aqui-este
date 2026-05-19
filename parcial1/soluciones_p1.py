7d318a6d03858348a8cdb10838c4a5d922a73c57
nick-290811 <nicolas.salgado@est.uexternado.edu.co>
2026-05-18 21:47:42 -0500

    agregado parcial 1 punto 2

--- parcial1/soluciones_p1.py
+++ parcial1/soluciones_p1.py
@@ -7,3 +7,17 @@ def f1(func, a, b):
     return total
 
 # f1(lambda x: x**2, 1, 3)  # Ejemplo de uso: sumatoria de 1^2 * 1 + 2^2 * 2 + 3^2 * 3 para i entre 1 y 3
+
+# Una función `f2(L)` recibe una lista de números y retorna una función `f(x)` 
+# que representa el polinomio con coeficientes en `L` evaluado en `x`.
+def f2(L):
+    def f(x):
+        total = 0
+        for i in range(len(L)):
+            total += L[i] * (x ** i)
+        return total
+    return f
+
+# Ejemplo de uso: si L = [1, 2, 3], entonces f(x) = 1 + 2*x + 3*x^2
+# polinomio = f2([1, 2, 3])
+# print(polinomio(2))  # Evaluar el polinomio en x=2, debería retornar 1 + 2*2 + 3*2^2 = 1 + 4 + 12 = 17
