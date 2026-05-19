8d6fa2ad4e30979c06cde2cb441ad7cfa9afe559
nick-290811 <nicolas.salgado@est.uexternado.edu.co>
2026-05-18 21:41:22 -0500

    agregado parcial 2 punto 2

--- parcial2/soluciones_p2.py
+++ parcial2/soluciones_p2.py
@@ -1,3 +1,17 @@
-"""
-Soluciones Parcial 2: Generadores, excepciones y recursión.
-"""
+def f2(func):
+    error_count = 0
+    
+    def wrapper(*args, **kwargs):
+        nonlocal error_count
+        if error_count >= 3:
+            print("> Leer la documentacion")
+            return None
+        
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            error_count += 1
+            print(f"> Ha cometido {error_count} error(es): {type(e).__name__}")
+            return None
+            
+    return wrapper
